09dd49b766c7e2860738e30d7d1e7aa8477af961
katipallyshivanireddy <katipallyshivanireddy1@gmail.com>
2024-11-08 12:47:29 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,25 @@
+let d=document.getElementById("p")
+let k=document.getElementById("btn")
+
+
+function b(){
+    k.textContent="this is button"
+    k.style.fontSize="60px"
+    k.style.backgroundColor="red"
+    k.style.borderRadius="50%"
+    
+}
+
+
+var t=document.getElementById("button");
+var a=document.getElementById("para")
+function z(){
+    a.style.borderRadius="30px"
+    a.style.display="block"
+    a.innerHTML=` <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg" style="border-radius:30px"/>
+    <img src="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg" style="border-radius:100px" />
+    <img src="https://www.shutterstock.com/shutterstock/photos/1032541603/display_1500/stock-photo-the-young-and-conceptual-image-of-a-large-stone-in-the-shape-of-the-human-brain-1032541603.jpg" style="border-radius:50%"/> `
+   
+    
+
+}
